3b4aa88976730746cf9a056fdaec9a3d46d0128b
Sudeep-sm <noobadum00@gmail.com>
2025-04-09 09:03:43 +0530

    save

--- java/Add.java
+++ java/Add.java
@@ -4,8 +4,10 @@ public class Add {
                     public static void main(String[] args) {
                         int a = 10;
                         int b = 5;
-                        int mul = a * b;
-                        System.out.println("The sum is: " + mul);
+                        int sum = a + b;
+                        int sub = a - b;
+                        System.out.println("The sum is: " + sum);
+                        System.out.println("The sum is: " + sum);
                     }
                 }
                 
